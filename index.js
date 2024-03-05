0313baa3396c424b003e57535aaba5748cca607a
Sajid Hussain <sajid6207116@gmail.com>
2024-03-05 16:21:53 +0500

    Project done

---
+++ index.js
@@ -0,0 +1,16 @@
+import data from "./data/quiz.json" assert {type: "json"}
+let optno = ['a', 'b', 'c', 'd']
+
+for(let i=0; i<data.length; i++){
+    console.log("Q."+data[i].id+ ": "+ data[i].statement);
+    for(let j=0; j<data[i].options.length; j++){
+        let alphabet = optno[j]
+        if(data[i].answer == data[i].options[j]){
+            console.log("\t"+ alphabet+ ". *"+ data[i].options[j])
+        }
+        else {
+            console.log("\t"+ alphabet+ ". "+ data[i].options[j])
+        }
+    }
+}
+
